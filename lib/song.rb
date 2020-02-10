bab74ec285f15769634e167c1fce1ccf9f481141
Maurice Fiorani <mauricef97@gmail.com>
2020-02-10 04:52:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,39 +1,4 @@
-class Song 
-  attr_accessor :name, :artist, :genre 
-  @@count = 0 
-  @@genres = []
-  @@artists = []
-  
-  def initialize(song_name, artist, genre)
-    @name = song_name
-    @artist = artist
-    @genre = genre
-    @@count += 1
-    @@genres << genres
-    @@artists << artist
-  end
-  
-  def self.count
-    @@count
-  end
-  
-  def self.genres
-    @@genres.uniq!
-  end
-  
-  def self.artists
-    @@artists.uniq!
-  end
-  
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] +=1
-      else
-        genre_count[genre] = 1 
-      end
-    end
+
     genre_count
   end
   
